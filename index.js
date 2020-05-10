cad58ef30f713bac857abe576c142a8a4e58e672
jgam <jgam@alumni.nd.edu>
2020-05-10 21:20:25 +0900

    function sayHi in ts

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log('hello');
+const name = 'jgam', age = 24, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}.`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
 console.log('hello');
+
+const name = 'jgam',
+  age = 24,
+  gender = 'male';
+
+const sayHi = (name, age, gender) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}.`);
+};
+
+sayHi(name, age, gender);
+
+//this is typescript thing
+export {};
